1a8c8545720caa324c34a1e3f6573ef88097fc95
YOGANANDH60 <yoga132005@gmail.com>
2026-03-15 21:41:02 +0530

    password rest

--- blog/settings.py
+++ blog/settings.py
@@ -134,3 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login' 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'yoga132005@gmail.com'
+EMAIL_HOST_PASSWORD = 'aams atyy giho ixny'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
